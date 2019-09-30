3bb859154b300c8ebe85b751f7c77ba02ec391eb
Austin Traver <austintraver@users.noreply.github.com>
2019-09-29 20:41:43 -0700

    Simplify syntax of macd.yml
    Previously, the syntax specified some features that had yet to be
    implemented. This update removes the unimplemented features from
    the syntax

--- macd.py
+++ macd.py
@@ -1,8 +1,8 @@
 from xylem.functions.metrics import alpha
 from pendulum import from_format
 
-def compute(context, stats, order):
 
+def compute(context, stats, order):
     '''
     1. Find the current amount of buying power
     2. Find the current price of the security
@@ -11,38 +11,42 @@ def compute(context, stats, order):
     '''
 
     # If short term momentum exceeds long term momentum
-    if (stats["EMA12"] * (1.00 - context["bias"])) > stats["EMA26"] and not context["long_position"]:
+    if ((stats["EMA12"] * (1.00 - context["bias"])) > stats["EMA26"] and
+            not context["long_position"]):
         # Enter a long position, selling our short position if we have one
         if context["short_position"]:
-            # Returning the stock that we borrowed requires subtracting from our balance
-            order(1, all_in=True)
+            # Exit the short position with a market buy order
+            order(1)
             context["short_position"] = False
 
-        order(1, all_in=True)
+        order(1)
         context["long_position"] = True
 
     # If short term momentum falls below long term momentum
-    elif (stats["EMA12"] * (1.00 + context["bias"])) < stats["EMA26"] and not context["short_position"]:
+    elif ((stats["EMA12"] * (1.00 + context["bias"])) < stats["EMA26"] and
+          not context["short_position"]):
         # Enter a short position, selling our long position if we have one
         if context["long_position"]:
-            order(-1, all_in=True)
+            order(-1)
             context["long_position"] = False
 
-        order(-1, all_in=True)
+        order(-1)
         context['short_position'] = True
 
-def before_exit(context, stats, order):
-    if context["long_position"]:
-        print("Long position before_exit()")
 
-    if context["short_position"]:
-        print("Short position before_exit()")
+def before_exit(context, stats, order):
+    # if context["long_position"]:
+        # print("Long position before_exit()")
+
+    # if context["short_position"]:
+        # print("Short position before_exit()")
 
     order(liquidate=True)
 
+
 def analyze(result_set, barsets):
-    for r, barset in zip(result_set, barsets):
-        RI = r.ending_balance - r.starting_balance
-        start = from_format(r.start_time, 'YYYY-MM-DD').date()
-        stop = from_format(r.stop_time, 'YYYY-MM-DD').date()
-        print("Alpha: ", alpha(RI, r.ticker, start, stop))
+    for results, barset in zip(result_set, barsets):
+        RI = results["ending_balance"] - results["starting_balance"]
+        start = from_format(results["start_time"], 'YYYY-MM-DD').date()
+        stop = from_format(results["stop_time"], 'YYYY-MM-DD').date()
+        # print("Alpha: ", alpha(RI, results.symbol, start, stop))
